b4b23d00a679a24a476b597bcf058145d35c288d
OysterKing <comhghall.mc-keating@ucd.ie>
2014-07-04 15:15:27 +0100

    Added new png files for the banners of the north. Added a new activity created when the user enters a guess which will eventually display the next banner in the list.

--- src/com/example/bannersofwesteros/DisplayNextBanner.java
+++ src/com/example/bannersofwesteros/DisplayNextBanner.java
@@ -1,22 +1,22 @@
 package com.example.bannersofwesteros;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
+import android.os.Build;
 
-public class DisplayBannerActivity extends ActionBarActivity {
+public class DisplayNextBanner extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_display_banner);
+		setContentView(R.layout.activity_display_next_banner);
 
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
@@ -28,7 +28,7 @@ public class DisplayBannerActivity extends ActionBarActivity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.display_banner, menu);
+		getMenuInflater().inflate(R.menu.display_next_banner, menu);
 		return true;
 	}
 
@@ -55,21 +55,10 @@ public class DisplayBannerActivity extends ActionBarActivity {
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_display_banner,
-					container, false);
+			View rootView = inflater.inflate(
+					R.layout.fragment_display_next_banner, container, false);
 			return rootView;
 		}
-	}
-	
-	public void nextBanner(View view){
-		Intent intent = new Intent(this, DisplayNextBanner.class);
-		EditText editText = (EditText) findViewById(R.id.editText1);
-		String house_guess = editText.getText().toString(); 
-		
-//		if(house_guess == "Stark"){
-			startActivity(intent);
-//		}
-		
 	}
 
 }
